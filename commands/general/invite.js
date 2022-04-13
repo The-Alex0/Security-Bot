502b32e6f8030195a373fa83d27b617227826e3a
BlackMan <102436893+The-BlackMan@users.noreply.github.com>
2022-04-14 00:35:04 +0300

    Update invite.js

--- commands/general/invite.js
+++ commands/general/invite.js
@@ -15,7 +15,7 @@ module.exports = {
  let butn = new MessageButton()
   .setStyle('url')
   .setURL('https://discord.com/api/oauth2/authorize?client_id=956981187996434532&permissions=8&scope=bot') 
-  .setEmoji('<:emoji_97:963873812510810193>')
+  .setEmoji('963873812510810193')
   .setLabel('Invite Evil Bot')
 
 return message.channel.send(`Hello Sir Are You Sure You Can Invite Me To Secure Your Server?? Good Click Below To Invite Me.`, butn);
