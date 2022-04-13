844703bf2b3478ea430edd66cc8dba06ff890d93
BlackMan <102436893+The-BlackMan@users.noreply.github.com>
2022-04-14 00:38:14 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ require('discord-buttons')(bot);
 const { Color, Image, Footer, Author } = require("./config.js");
 const fs = require("fs"); 
 const request = require("request");
-const prefix = "e!";
+const prefix = "!";
 const { Collection, MessageEmbed } = require("discord.js");
 const { inspect } = require("util");
 let dev = ["802491275445010443"];
